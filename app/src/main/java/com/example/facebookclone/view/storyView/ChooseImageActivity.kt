f686ad3c646bdc67d76a5039c9e7a807f90b98ed
nickevan9 <55218432+nickevan9@users.noreply.github.com>
2022-04-23 10:46:00 +0700

    story view

---
+++ app/src/main/java/com/example/facebookclone/view/storyView/ChooseImageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.facebookclone.view.storyView
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.facebookclone.R
+
+class ChooseImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_image)
+    }
+}
